3cbec3b08f9fb17c335a1bc5ab7d7ba1db7c3718
budryte-liolaitiene <gintare.budryte@stud.techin.lt>
2026-05-14 21:34:29 +0300

    sukurta failu struktura

---
+++ frontend/src/services/api_example.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// Naudojame .env kintamąjį baziniam URL (pvz., http://localhost:8000)
+const API_URL = import.meta.env.VITE_API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
