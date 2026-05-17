fbd33fa28a0be41d21a997201f67993fe273c228
budryte-liolaitiene <gintare.budryte@stud.techin.lt>
2026-05-17 20:14:53 +0300

    expanding testing in example.py

--- backend/example.py
+++ backend/example.py
@@ -1,25 +1,36 @@
 from app.services.db_connection import DatabaseManager
 
+
 def test_database_connection():
     print("=== Duomenų bazės testavimo pradžia ===")
 
     db = DatabaseManager()
-    
+
+    print("Prisijungiama prie duomenų bazės...")
+
     try:
         print("Bandoma atlikti bandomąją užklausą...")
-        result = db.fetch_all("SELECT 1 AS test_connection")
-        
-        print(f"Sėkmė! Atsakymas iš DB: {result}")
-        print("Prisijungimas veikia puikiai, viskas grąžinama žodyno (dict) formatu!")
-        
+        print("Laukiami visų lentelių pavadinimai iš duomenų bazės...")
+        result_tables = db.fetch_all("SHOW TABLES;")
+
+        print(f"Sėkmė! \nAtsakymas iš DB: {result_tables}")
+
+        print("\n\n Laukiami visi įrašai iš 'users' lentelės...")
+        result_users = db.fetch_all("SELECT * FROM users;")
+        print(f"Sėkmė! \nAtsakymas iš DB: {result_users}")
+
+        print("Ar atsakymas yra sąrašas? ", isinstance(result_users, list))
+
+        print("Bandoma atlikti įterpimo užklausą į 'users' lentelę...")
+
     except Exception as e:
         print(f"❌ Klaida testuojant duomenų bazę: {e}")
         print("Patikrinkite, ar veikia jūsų MySQL serveris ir ar teisingi .env duomenys.")
-        
+
     finally:
-        # 3. Visada tvarkingai uždarome ryšį po testo
         db.close_connection()
         print("=== Testas baigtas, ryšys uždarytas ===")
+
 
 if __name__ == "__main__":
     test_database_connection()
