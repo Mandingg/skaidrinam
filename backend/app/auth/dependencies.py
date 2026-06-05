f61203806ce51a64674ac06cada8db5e6aa9a8f0
Benita Ulytė <benitaulyte03@gmail.com>
2026-06-05 19:09:28 +0300

    patrumpintas kodas

--- backend/app/auth/dependencies.py
+++ backend/app/auth/dependencies.py
@@ -4,33 +4,6 @@ from app.auth.jwt_handler import verify_token
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login", auto_error=False)
 
-# def get_current_user(token: str = Depends(oauth2_scheme)):
-#     if not token:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED, 
-#             detail="Prisijungimo žetonas nepateiktas"
-#         )
-
-#     if token in ("null", "undefined"):
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED, 
-#             detail="Vartotojas neprisijungęs (token yra null/undefined)"
-#         )
-
-#     try:
-#         payload = verify_token(token)
-#         if not payload:
-#             raise HTTPException(
-#                 status_code=status.HTTP_401_UNAUTHORIZED, 
-#                 detail="Neteisingas arba pasibaigusio galiojimo žetonas"
-#             )
-#         return payload
-        
-#     except Exception:
-#         raise HTTPException(
-#             status_code=status.HTTP_401_UNAUTHORIZED, 
-#             detail="Neteisingas arba pasibaigusio galiojimo žetonas"
-#         )
 def get_current_user(authorization: str = Header(None)):
     if not authorization:
         raise HTTPException(
